bdc6afbc5fba1de8d6eebebb43929f0cab5821d9
Isaac Roque <isaacroque0209@gmail.com>
2024-03-09 18:39:59 -0300

    Obtendo um usuário por id

--- src/main/java/com/isaac/workshopmongo/resources/UserResources.java
+++ src/main/java/com/isaac/workshopmongo/resources/UserResources.java
@@ -7,6 +7,7 @@ import java.util.stream.Collectors;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -27,6 +28,10 @@ public class UserResources {
 		List<UserDTO> list = service.findAll().stream().map(x -> new UserDTO(x)).collect(Collectors.toList());
 
 		return ResponseEntity.ok().body(list);
+	}
+	@GetMapping("/{id}")
+	public ResponseEntity<User> findById(@PathVariable String id){
+		return ResponseEntity.ok().body(service.findById(id));
 	}
 
 }
